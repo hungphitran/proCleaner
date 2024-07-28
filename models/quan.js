e51918dae6159780574481ab39ed9ae2124edb89
hungphitran <hungphitran.22@gmail.com>
2024-07-28 18:48:25 +0700

    'up-to-date'

--- models/quan.js
+++
@@ -1,9 +0,0 @@
-﻿//dependencies
-const restful = require('node-restful');
-const mongoose = restful.mongoose;
-
-const QuanSchema = new mongoose.Schema({
-	tenquan : String,
-   	khuvuc : String
-})
-module.exports = restful.model('quan',QuanSchema);
